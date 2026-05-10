fb73e48fe2a3213d41fe17dd4763186e361a3f59
Abdallah Kareem <162487911+abdallahkareem@users.noreply.github.com>
2026-05-10 09:49:20 +0300

    Update trainer to use DeepFace and save embeddings
    Refactor model training to use DeepFace for embeddings and save trained data to a pickle file.

--- 3_Model_Training/trainer.py
+++ 3_Model_Training/trainer.py
@@ -1,66 +1,49 @@
-import cv2
 import os
-import numpy as np
+import pickle
+from deepface import DeepFace
 
-# =========================
-# Dataset Path
-# =========================
-path = r"D:\hady\3nd year\Second_term\Supervised_Learning\project\Face-Recognition-System\processed_dataset"
+dataset_path = "datasets"
 
-# =========================
-# Create Recognizer
-# =========================
-recognizer = cv2.face.LBPHFaceRecognizer_create(
-    radius=1,
-    neighbors=8,
-    grid_x=8,
-    grid_y=8
-)
+embeddings = []
+names = []
 
-faces = []
-ids = []
+# loop on persons
+for person_name in os.listdir(dataset_path):
 
-label_map = {}
-current_id = 0
+    person_folder = os.path.join(dataset_path, person_name)
 
-for person_name in os.listdir(path):
-
-    person_path = os.path.join(path, person_name)
-
-    if not os.path.isdir(person_path):
+    if not os.path.isdir(person_folder):
         continue
 
-    label_map[current_id] = person_name
+    # loop on images
+    for image_name in os.listdir(person_folder):
 
-    print(f"Training for: {person_name} (ID: {current_id})")
+        image_path = os.path.join(person_folder, image_name)
 
-    for image_name in os.listdir(person_path):
+        try:
+            # extract embedding using FaceNet
+            embedding = DeepFace.represent(
+                img_path=image_path,
+                model_name="Facenet",
+                enforce_detection=False
+            )
 
-        image_path = os.path.join(person_path, image_name)
+            embeddings.append(embedding[0]["embedding"])
+            names.append(person_name)
 
-        # Read grayscale image
-        gray_img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+            print(f"Added: {person_name} - {image_name}")
 
-        if gray_img is None:
-            continue
+        except Exception as e:
+            print(f"Error in {image_path}")
+            print(e)
 
-        # IMPORTANT:
-        # image is already cropped face
-        faces.append(gray_img)
+# save trained data
+data = {
+    "embeddings": embeddings,
+    "names": names
+}
 
-        ids.append(current_id)
+with open("facenet_trained.pkl", "wb") as f:
+    pickle.dump(data, f)
 
-    current_id += 1
-
-if len(faces) > 0:
-
-    recognizer.train(faces, np.array(ids))
-
-    recognizer.write("trainer.yml")
-
-    print("\nModel Trained Successfully!")
-
-    print(f"Total Faces: {len(faces)}")
-
-else:
-    print("Error: No images found!")
+print("Training Finished")
